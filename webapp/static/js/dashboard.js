eb846c107d14c83eacda6d7be821a5fdd7f006a0
mandela <fasemoremandela@gmail.com>
2019-03-28 16:09:47 +0100

    modified dashboard no url

--- webapp/static/js/dashboard.js
+++ webapp/static/js/dashboard.js
@@ -1,76 +1,22 @@
 $(document).ready(function()
 {
-//	display_href()
-	get_data()
-	
-})
 
-function display_href(){
-	var ref = window.location.href
-	var dom_ = window.location.hostname
-	var pat = window.location.pathname.split("/")
-	alert(pat[2])
-	
-
-
-
-
-}
-function get_data(){
-
-
-	// get data
-	var url_path = window.location.pathname.split("/")[2]
-	var url = "http://localhost:6543/api/" + url_path
-
-	$.get(url, 'json').done(function(results) {
-		drawchart(results)
-	}).fail(function (e){
-		if (e.error) {
-			alert("error due to" + e.error)
-		}
-		else {
-			alert("fail")
-		}
-		})
-
-}
-
-
-	
-function drawchart(result) {
-	// get key value
-	item = JSON.parse(JSON.stringify(result))
-	var i, label = [], values = [];
-	for(var key in item) label.push(key);
-	for (var key in item) values.push(item[key]);
-	
-	// get element
 	var ctx = $('#barchart')[0].getContext('2d');
 	ctx.canvas.width = 700;
 	ctx.canvas.height = 500;
 	myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: label,
+        labels: null,
         datasets: [{
             label: '# of Isolates',
-            data: values,
-            backgroundColor: "#3e95cd" // "#8e5ea2","#3cba9f","#e8c3b9","#c45850"
-            ,
-          //  borderColor: [
-          //      'rgba(255,99,132,1)',
-          //      'rgba(54, 162, 235, 1)',
-          //      'rgba(255, 206, 86, 1)',
-          //      'rgba(75, 192, 192, 1)',
-          //      'rgba(153, 102, 255, 1)',
-          //      'rgba(255, 159, 64, 1)'
-          //  ],
+            data: null,
+            backgroundColor: "#3e95cd",
             borderWidth: 1
         }]
     },
     options: {
-	responsive: false,
+	responsive: true,
 	maintainAspectRatio: false,
         scales: {
             yAxes: [
@@ -94,49 +40,139 @@ function drawchart(result) {
         }
     }
 });
-	
 
-}
+	var url = "http://localhost:6543/api/" + "SampleHost"
 
-function update_chart(result) {
-	// get key value
-	item = JSON.parse(JSON.stringify(result))
-	var i, label = [], values = [];
-	for(var key in item) label.push(key);
-	for (var key in item) values.push(item[key]);
-	myChart.data.labels = label;
-	myChart.data.datasets.data = values;
-	myChart.update();
-}
+	$.get(url, 'json').done(function(results) {
+		item = JSON.parse(JSON.stringify(results))
+		var i, label = [], values = [];
+		for(var key in item) label.push(key);
+		for (var key in item) values.push(item[key]);
+		myChart.data.labels = label;
+		myChart.data.datasets[0].data = values;
+		myChart.update();
+	}).fail(function (e){
+		if (e.error) {
+			alert("error due to" + e.error)
+		}
+		else {
+			alert("fail")
+		}
+		})
 
-//$( "button" ).click(function() {
-//	var cond = $(this).attr("value");
-//	var url = "http://localhost:6543/api/" + cond
-//
-//	$.get(url, 'json').done(function(results) {
-//		update_chart(results)
-//
-//		
-//	}).fail(function (e){
-//		if (e.error) {
-//			alert("error due to" + e.error)
-//		}
-//		else {
-//			alert("fail")
-//		}
-//		})
-//	
-//
-//});
-//
-//  
-////function draw_filter_chart(){
-////      // get current url
-////	var url_path = window.location.pathname.split("/")[2]
-//	var cond = $(this).value
-//	var url = "http://localhost:6543/api/" + url_path
-//	alert(cond)
-//}
-//	// add filter
-//	// get data
-//	// make plot
+
+$("#SampleYear").on ("click", function () {
+	var url_path = window.location.pathname.split("/")[2]
+	var url = "http://localhost:6543/api/" + "SampleYear"
+
+	$.get(url, 'json').done(function(results) {
+		item = JSON.parse(JSON.stringify(results))
+		var i, label = [], values = [];
+		for(var key in item) label.push(key);
+		for (var key in item) values.push(item[key]);
+		myChart.data.labels = label;
+		myChart.data.datasets[0].data = values;
+		myChart.update();
+	}).fail(function (e){
+		if (e.error) {
+			alert("error due to" + e.error)
+		}
+		else {
+			alert("fail")
+		}
+		})
+
+});
+
+$("#SampleHost").on ("click", function () {
+	var url_path = window.location.pathname.split("/")[2]
+	var url = "http://localhost:6543/api/" + "SampleHost"
+
+	$.get(url, 'json').done(function(results) {
+		item = JSON.parse(JSON.stringify(results))
+		var i, label = [], values = [];
+		for(var key in item) label.push(key);
+		for (var key in item) values.push(item[key]);
+		myChart.data.labels = label;
+		myChart.data.datasets[0].data = values;
+		myChart.update();
+	}).fail(function (e){
+		if (e.error) {
+			alert("error due to" + e.error)
+		}
+		else {
+			alert("fail")
+		}
+		})
+
+});
+$("#SampleCountry").on ("click", function () {
+	var url_path = window.location.pathname.split("/")[2]
+	var url = "http://localhost:6543/api/" + "SampleCountry"
+
+	$.get(url, 'json').done(function(results) {
+		item = JSON.parse(JSON.stringify(results))
+		var i, label = [], values = [];
+		for(var key in item) label.push(key);
+		for (var key in item) values.push(item[key]);
+		myChart.data.labels = label;
+		myChart.data.datasets[0].data = values;
+		myChart.update();
+	}).fail(function (e){
+		if (e.error) {
+			alert("error due to" + e.error)
+		}
+		else {
+			alert("fail")
+		}
+		})
+
+});
+$("#TypingID").on ("click", function () {
+	var url_path = window.location.pathname.split("/")[2]
+	var url = "http://localhost:6543/api/" + "TypingID"
+
+	$.get(url, 'json').done(function(results) {
+		item = JSON.parse(JSON.stringify(results))
+		var i, label = [], values = [];
+		for(var key in item) label.push(key);
+		for (var key in item) values.push(item[key]);
+		myChart.data.labels = label;
+		myChart.data.datasets[0].data = values;
+		myChart.update();
+	}).fail(function (e){
+		if (e.error) {
+			alert("error due to" + e.error)
+		}
+		else {
+			alert("fail")
+		}
+		})
+
+});
+$("#CountryProvince").on ("click", function () {
+	var url_path = window.location.pathname.split("/")[2]
+	var url = "http://localhost:6543/api/" + "CountryProvince"
+
+	$.get(url, 'json').done(function(results) {
+		item = JSON.parse(JSON.stringify(results))
+		var i, label = [], values = [];
+		for(var key in item) label.push(key);
+		for (var key in item) values.push(item[key]);
+		myChart.data.labels = label;
+		myChart.data.datasets[0].data = values;
+		myChart.update();
+	}).fail(function (e){
+		if (e.error) {
+			alert("error due to" + e.error)
+		}
+		else {
+			alert("fail")
+		}
+		})
+
+});
+
+
+});	
+
