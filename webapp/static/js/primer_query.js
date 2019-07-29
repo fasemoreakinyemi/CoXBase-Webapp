17cf6771e16935495f8ca3c2267a9ae259df241c
Ubuntu <ubuntu@gitlab.q-gaps-de>
2019-07-29 16:07:50 +0000

    inluded all primer query

---
+++ webapp/static/js/primer_query.js
@@ -0,0 +1,68 @@
+$(document).ready(function()
+{
+
+$( "#primer_form" ).submit(function( event ) {
+	event.preventDefault();
+	var wanted = [];
+	$.each($('input[name="primer"]:checked'), function(){wanted.push($(this).val());});
+	if (wanted.length === 0) {
+	throw_empty_error()
+	}
+	else{
+		var hst = location.host;
+		var url = "http://" + hst + "/webapp/primer_results/" + wanted.join("/")
+		window.open(url, "_self")
+	}
+})
+});
+
+
+function throw_empty_error(){
+	$('#result').empty()
+	$('#empty_error').html("<div class='container'><div class='well blue'><span class='error_well'><i class='fas fa-exclamation-triangle fa-2x'></i></span></div><div class='well white'>You did not select any method</div></div>")
+}
+
+function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Hide download link
+    downloadLink.style.display = "none";
+
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+
+    // Click download link
+    downloadLink.click();
+}
+
+function exportprimertableToCSV(filename) {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+
+        for (var j = 0; j < cols.length; j++)
+            row.push(cols[j].innerText);
+
+        csv.push(row.join(","));
+    }
+
+    // Download CSV file
+    downloadCSV(csv.join("\n"), filename);
+}
+
+
