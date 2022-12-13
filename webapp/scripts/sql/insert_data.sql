15ee825b618518c37916a590fc25f5b3dfd26140
Fasemore A. Mandela <27198406+fasemoreakinyemi@users.noreply.github.com>
2022-12-13 21:56:53 +0100

    Update insert_data.sql

--- webapp/scripts/sql/insert_data.sql
+++ webapp/scripts/sql/insert_data.sql
@@ -1,56 +1,56 @@
 
 USE MLVA;
 
-LOAD DATA LOCAL INFILE '/home/travis/build/foerstner-lab/CoxBase-Webapp/webapp/scripts/sql/sql_tables/adaA_analyses.csv'
+LOAD DATA LOCAL INFILE 'webapp/scripts/sql/sql_tables/adaA_analyses.csv'
 INTO TABLE adaAProfile
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/travis/build/foerstner-lab/CoxBase-Webapp/webapp/scripts/sql/sql_tables/is1111_analyses.csv'
+LOAD DATA LOCAL INFILE 'webapp/scripts/sql/sql_tables/is1111_analyses.csv'
 INTO TABLE is1111Profile
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/travis/build/foerstner-lab/CoxBase-Webapp/webapp/scripts/sql/sql_tables/mlva_analyses.csv'
+LOAD DATA LOCAL INFILE 'webapp/scripts/sql/sql_tables/mlva_analyses.csv'
 INTO TABLE ProductLengthISPCR
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/travis/build/foerstner-lab/CoxBase-Webapp/webapp/scripts/sql/sql_tables/mst_analyses.csv'
+LOAD DATA LOCAL INFILE 'webapp/scripts/sql/sql_tables/mst_analyses.csv'
 INTO TABLE mstSpacerResultTable
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/travis/build/foerstner-lab/CoxBase-Webapp/webapp/scripts/sql/sql_tables/isolates.csv'
+LOAD DATA LOCAL INFILE 'webapp/scripts/sql/sql_tables/isolates.csv'
 INTO TABLE isolates
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/travis/build/foerstner-lab/CoxBase-Webapp/webapp/scripts/sql/sql_tables/mlva_query.csv'
+LOAD DATA LOCAL INFILE 'webapp/scripts/sql/sql_tables/mlva_query.csv'
 INTO TABLE mlva_normalized
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/travis/build/foerstner-lab/CoxBase-Webapp/webapp/scripts/sql/sql_tables/mst_query.csv'
+LOAD DATA LOCAL INFILE 'webapp/scripts/sql/sql_tables/mst_query.csv'
 INTO TABLE mstgroups2
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/travis/build/foerstner-lab/CoxBase-Webapp/webapp/scripts/sql/sql_tables/geotables.csv'
+LOAD DATA LOCAL INFILE 'webapp/scripts/sql/sql_tables/geotables.csv'
 INTO TABLE isolates_geolocation
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"' 
